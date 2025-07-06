c5e07565dc10c4d51e23003f4aed68c2162dc94d
camilogamerytty <camiloarevaloroa4@gmail.com>
2025-07-06 17:27:02 -0500

    inicio de sesion y registro funcional y eliminacion de archivos de prueba
    esta el aplicativo en la carpeta de codificacion el cual funciona registrando usuarios y iniciando la sesion sin poderse ver los usuarios

---
+++ codificacion/app/controllers/main_controller.py
@@ -0,0 +1,9 @@
+# app/controllers/main_controller.py
+from flask import Blueprint, render_template
+
+main_bp = Blueprint('main_bp', __name__)
+
+@main_bp.route('/')
+def home():
+    return render_template('registro.html')
+
---
+++ codificacion/app/controllers/user_controller.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, request, redirect, url_for, current_app, session
+from flask_bcrypt import Bcrypt
+from werkzeug.utils import secure_filename
+import os
+
+from app.models.user_model import get_user_by_email, insert_user
+
+user_bp = Blueprint('user_bp', __name__)
+bcrypt = Bcrypt()
+
+@user_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        connection = current_app.connection
+        user = get_user_by_email(connection, email)
+        if user and bcrypt.check_password_hash(user['contraseña'], password):
+            session['user_id'] = user['id']
+            return redirect(url_for('user_bp.inicio'))
+        else:
+            return "Login Failed"
+
+    return render_template('inicio_sesion.html')
+
+@user_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    connection = current_app.connection
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+
+        insert_user(connection, name, email, password)
+        return redirect(url_for('user_bp.login'))
+    return render_template('registro.html')
+
+@user_bp.route('/inicio')
+def inicio():
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('user_bp.login'))
+    return render_template('inicio.html')
+
+
+@user_bp.route('/logout')
+def logout():
+    session.clear()
+    return render_template('inicio.html')
