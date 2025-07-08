e44a79cd10cec0b3e53928c9cdce025ed56fd784
camilogamerytty <camiloarevaloroa4@gmail.com>
2025-07-07 23:41:01 -0500

    agrego la primera version florte 1.0.0
    en esta funciona el registro y inicio de sesion y estan conectados todos las paginas

--- codificacion/app/controllers/user_controller.py
+++ codificacion/app/controllers/user_controller.py
@@ -8,6 +8,7 @@ from app.models.user_model import get_user_by_email, insert_user
 user_bp = Blueprint('user_bp', __name__)
 bcrypt = Bcrypt()
 
+
 @user_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -18,11 +19,12 @@ def login():
         user = get_user_by_email(connection, email)
         if user and bcrypt.check_password_hash(user['contraseña'], password):
             session['user_id'] = user['id']
-            return redirect(url_for('user_bp.inicio'))
+            return redirect(url_for('user_bp.feed'))
         else:
             return "Login Failed"
 
     return render_template('inicio_sesion.html')
+
 
 @user_bp.route('/register', methods=['GET', 'POST'])
 def register():
@@ -37,15 +39,52 @@ def register():
         return redirect(url_for('user_bp.login'))
     return render_template('registro.html')
 
-@user_bp.route('/inicio')
-def inicio():
+
+@user_bp.route('/feed')
+def feed():
     user_id = session.get('user_id')
     if not user_id:
         return redirect(url_for('user_bp.login'))
-    return render_template('inicio.html')
+    return render_template('feed.html')
+
+
+@user_bp.route('/perfil')
+def perfil():
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('user_bp.login'))
+    return render_template('perfil.html')
+
+
+@user_bp.route('/explorar')
+def explorar():
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('user_bp.login'))
+    return render_template('explorar.html')
+
+
+@user_bp.route('/afinidad')
+def afinidad():
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('user_bp.login'))
+    return render_template('afinidad.html')
+
+
+@user_bp.route('/mensajes')
+def mensajes():
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('user_bp.login'))
+    return render_template('mensajes.html')
 
 
 @user_bp.route('/logout')
 def logout():
     session.clear()
+<<<<<<< Updated upstream
     return render_template('inicio.html')
+=======
+    return render_template('feed.html')
+>>>>>>> Stashed changes
